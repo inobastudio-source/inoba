4fda9ce606e328aef12184d30e5efc49eef8a7bf
sguzmanr <sguzmanr2@gmail.com>
2025-09-19 07:05:32 -0500

    Debido a falta de pago, se bloquea la pagina web

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -16,6 +16,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <div className="w-screen h-screen fixed top-0 left-0 bg-red-500/80 z-50">
+        <div className="w-full h-full flex flex-col gap-2 items-center justify-center">
+          <h1 className="font-extrabold text-white text-4xl">PÁGINA WEB DESHABILITADA</h1>
+          <p className="font-base text-white text-xl">Debido a demoras en pagos, esta pagina web ha sido deshabilitada</p>
+        </div>
+      </div>
+      
       <body className={`${comfortaa.variable} antialiased`}>
         {children}
         <Footer />
